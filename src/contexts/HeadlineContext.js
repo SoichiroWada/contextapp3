0b63fedcecbfe602cb158894cc4a598fabb6fa1b
SoichiroWada <soichirowada@ipmulticast.net>
2023-08-16 02:00:55 +0900

    Revised!

--- src/contexts/HeadlineContext.js
+++ src/contexts/HeadlineContext.js
@@ -5,11 +5,9 @@ export const HeadlineContext = createContext(null);
 const HeadlineContextProvider = (props) => {
     const [ headline, setHeadline ] = useState("Context App")
     const handleClick = () => {
-        console.log(document.getElementById("navh1").innerHTML)
         let content = document.getElementById("navh1").innerHTML;
-        if (content === "Context App") {
-            setHeadline("Book Lists");
-        } else {setHeadline("Context App");}
+        console.log(content)
+        content === "Context App" ? setHeadline("Book Lists") : setHeadline("Context App");
     }
     return (
         <HeadlineContext.Provider value={{ headline, handleClick }}>
